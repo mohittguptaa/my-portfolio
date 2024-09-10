ce96b3f5a193ec21cfe51e38d5a27a5927a94cf9
mohittguptaa <mohitgupta8218@gmail.com>
2024-09-10 18:06:13 +0530

    add a new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import techmyte from "../../Assets/Projects/zeecare.png";
 import mistore from "../../Assets/Projects/mistore.png";
 import cryptox from "../../Assets/Projects/cryptox.png";
+import Tracker from "../../Assets/Projects/tracker.png";
 
 function Projects() {
   return (
@@ -18,14 +19,15 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+          
+        <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={mistore}
+              imgPath={Tracker}
               isBlog={false}
-              title="Mi-Store-Clone"
-              description="Explore the world of e-commerce with our React-based project, a clone of Mi Store India. Dive into the realm of web development while mastering React skills. Discover how to replicate the functionality of a popular online store, honing your coding abilities in the process. Learn, create, and innovate with us!"
-              ghLink="https://github.com/mohittguptaa/MI-store"
-              demoLink="https://mi-store-one.vercel.app"
+              title="Real Time Device Tracking App "
+              description="Developed a real-time device tracking application using Node.js, Express, EJS, Leaflet, and Socket.io, enabling continuous.location monitoring with high accuracy.Integrated geolocation tracking and real-time map updates utilizing watchPosition and Leaflet, enhancing user interaction and live device tracking."
+              ghLink="https://github.com/mohittguptaa/Realtime-Location-Tracker"
+              demoLink=""
             />
           </Col>
 
@@ -47,6 +49,16 @@ function Projects() {
               description="crypto app revolutionizes the way users engage with digital currencies. Offering real-time exchange rates and dynamic graphs for all coins, it provides comprehensive insights at your fingertips. With seamless responsiveness across devices, stay informed and make informed decisions in the fast-paced world of cryptocurrency trading."
               ghLink="https://github.com/mohittguptaa/cryptox"
               demoLink="https://cryptox-india.vercel.app"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={mistore}
+              isBlog={false}
+              title="Mi-Store-Clone"
+              description="Explore the world of e-commerce with our React-based project, a clone of Mi Store India. Dive into the realm of web development while mastering React skills. Discover how to replicate the functionality of a popular online store, honing your coding abilities in the process. Learn, create, and innovate with us!"
+              ghLink="https://github.com/mohittguptaa/MI-store"
+              demoLink="https://mi-store-one.vercel.app"
             />
           </Col>
         </Row>
