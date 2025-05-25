ae6fd698987b1c8491c8d90ddb8ebca9ccd19b08
mohittguptaa <mohitgupta8218@gmail.com>
2025-05-25 10:45:10 +0530

    adding new projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,9 @@ import techmyte from "../../Assets/Projects/zeecare.png";
 import mistore from "../../Assets/Projects/mistore.png";
 import cryptox from "../../Assets/Projects/cryptox.png";
 import Tracker from "../../Assets/Projects/tracker.png";
+import Cosmos from "../../Assets/Projects/cosmos.png";
+import cvsion from "../../Assets/Projects/cvsion.png";
+import ypsm from "../../Assets/Projects/ypsm.png";
 
 function Projects() {
   return (
@@ -20,6 +23,35 @@ function Projects() {
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={cvsion}
+              isBlog={false}
+              title="Sunrising Cinevision Acadmy , jaipur website"
+              description="A dynamic website for Sunrising Cinevision Academy showcasing acting, dance, and modeling courses, featuring blog updates, responsive design, and a user-friendly interface to enhance student engagement and visibility."
+              demoLink="https://sunrisingcinevisionacademy.in"
+            />
+          </Col>
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ypsm}
+              isBlog={false}
+              title="Collage Wibsite for YPSM Hospital and medical College"
+              description="A professional website for YPSM Homeopathic and Medical College, highlighting academic programs, admissions, faculty, and campus facilities, designed to provide easy navigation and detailed institutional information for students."
+              ghLink=""
+              demoLink=""
+            />
+          </Col>
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Cosmos}
+              isBlog={false}
+              title="Cosmos Ai - A Real Time saterllite Tracking App "
+              description="Cosmos AI is a real-time satellite tracking app that visualizes satellite positions using interactive maps, delivering accurate, live orbital data with an intuitive and user-friendly interface."
+              ghLink=""
+              demoLink=""
+            />
+          </Col>
         <Col md={4} className="project-card">
             <ProjectCard
               imgPath={Tracker}
